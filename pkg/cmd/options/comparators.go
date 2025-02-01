60c638d9205abee2e8f803b735fac3d06427b8d6
JoukoVirtanen <jouko@stackrox.com>
2025-02-01 14:53:39 -0800

    Added the ability to select checks using wildcards

--- pkg/cmd/options/comparators.go
+++ pkg/cmd/options/comparators.go
@@ -2,6 +2,8 @@ package options
 
 import (
 	"fmt"
+	"path"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 
@@ -11,11 +13,13 @@ import (
 )
 
 type ComparatorOptions struct {
-	ComparatorRegistry        manifestcomparators.CRDComparatorRegistry
-	KnownComparators          []string
-	DefaultEnabledComparators []string
-	EnabledComparators        []string
-	DisabledComparators       []string
+	ComparatorRegistry          manifestcomparators.CRDComparatorRegistry
+	KnownComparators            []string
+	DefaultEnabledComparators   []string
+	EnabledComparators          []string
+	DisabledComparators         []string
+	DisabledComparatorWildcards []string
+	EnabledComparatorWildcards  []string
 }
 
 func NewComparatorOptions() *ComparatorOptions {
@@ -31,8 +35,8 @@ func NewComparatorOptions() *ComparatorOptions {
 }
 
 func (o *ComparatorOptions) AddFlags(fs *pflag.FlagSet) {
-	fs.StringSliceVar(&o.DisabledComparators, "disabled-validators", o.DisabledComparators, "list of comparators that must be disabled")
-	fs.StringSliceVar(&o.EnabledComparators, "enabled-validators", o.EnabledComparators, "list of comparators that must be enabled")
+	fs.StringSliceVar(&o.DisabledComparatorWildcards, "disabled-validators", o.DisabledComparatorWildcards, "list of comparators that must be disabled. May contain wildcards")
+	fs.StringSliceVar(&o.EnabledComparatorWildcards, "enabled-validators", o.EnabledComparatorWildcards, "list of comparators that must be enabled. May contain wildcards")
 }
 
 func (o *ComparatorOptions) Validate() error {
@@ -50,11 +54,42 @@ func (o *ComparatorOptions) Validate() error {
 	return nil
 }
 
+func (o *ComparatorOptions) comparatorWildcardsToComparators(comparatorWildcards []string) []string {
+	matchingComparators := make([]string, 0)
+
+	for _, known := range o.KnownComparators {
+		for _, wildcard := range comparatorWildcards {
+			if known == wildcard {
+				matchingComparators = append(matchingComparators, known)
+				break
+			}
+			if matched, _ := path.Match(wildcard, known); matched {
+				matchingComparators = append(matchingComparators, known)
+				break
+			}
+		}
+	}
+
+	return matchingComparators
+}
+
+func containsWildcards(comparatorWildcards []string) bool {
+	for _, wildcard := range comparatorWildcards {
+		if strings.ContainsAny(wildcard, "*") {
+			return true
+		}
+	}
+	return false
+}
+
 // Complete fills in missing values before command execution.
 func (o *ComparatorOptions) Complete() (*ComparatorConfig, error) {
 	ret := &ComparatorConfig{
 		ComparatorRegistry: o.ComparatorRegistry,
 	}
+
+	o.DisabledComparators = o.comparatorWildcardsToComparators(o.DisabledComparatorWildcards)
+	o.EnabledComparators = o.comparatorWildcardsToComparators(o.EnabledComparatorWildcards)
 
 	knownComparators := sets.NewString(o.KnownComparators...)
 	disabledComparators := sets.NewString(o.DisabledComparators...)
@@ -67,7 +102,25 @@ func (o *ComparatorOptions) Complete() (*ComparatorConfig, error) {
 		return nil, fmt.Errorf("unknown comparators: %v", disabledComparators.List())
 	}
 
-	comparatorsToRun := sets.NewString(o.DefaultEnabledComparators...).Insert(o.EnabledComparators...).Delete(o.DisabledComparators...)
+	disabledContainsWildcards := containsWildcards(o.DisabledComparatorWildcards)
+	enabledContainsWildcards := containsWildcards(o.EnabledComparatorWildcards)
+
+	comparatorsToRun := sets.NewString()
+
+	if disabledContainsWildcards {
+		comparatorsToRun = sets.NewString(o.DefaultEnabledComparators...).Delete(o.DisabledComparators...).Insert(o.EnabledComparators...)
+	} else if !disabledContainsWildcards && enabledContainsWildcards {
+		comparatorsToRun = sets.NewString(o.DefaultEnabledComparators...).Insert(o.EnabledComparators...).Delete(o.DisabledComparators...)
+	} else if len(o.EnabledComparators) == 0 && len(o.DisabledComparators) != 0 {
+		comparatorsToRun = sets.NewString(o.DefaultEnabledComparators...).Delete(o.DisabledComparators...)
+	} else if len(o.EnabledComparators) == 0 && len(o.DisabledComparators) == 0 {
+		comparatorsToRun = sets.NewString(o.DefaultEnabledComparators...)
+	} else if len(o.EnabledComparators) != 0 && len(o.DisabledComparators) == 0 {
+		return nil, fmt.Errorf("Enabling comparators without disabling comparators has no effect. Consider using a wildcard for enabled comparators")
+	} else if !disabledContainsWildcards && !enabledContainsWildcards {
+		return nil, fmt.Errorf("Cannot both disable and enable comparators if neither of them has a wildcard")
+	}
+
 	ret.ComparatorNames = comparatorsToRun.List()
 
 	return ret, nil
